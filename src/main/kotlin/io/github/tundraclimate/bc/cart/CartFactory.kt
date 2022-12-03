6d3d7b3891ec9a45bb15300d27b35ca458c53430
TundraClimate <tundra.tundere@gmail.com>
2022-12-04 00:11:52 +0900

    feat: added factory

---
+++ src/main/kotlin/io/github/tundraclimate/bc/cart/CartFactory.kt
@@ -0,0 +1,15 @@
+package io.github.tundraclimate.bc.cart
+
+import BooCart
+import org.bukkit.Location
+import org.bukkit.World
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
+import org.bukkit.metadata.FixedMetadataValue
+
+class CartFactory {
+    fun spawn(world: World, loc: Location, owner: Player) {
+        val cart = world.spawnEntity(loc, EntityType.PIG)
+        cart.setMetadata("BooCart:cart", FixedMetadataValue(BooCart.plugin, true))
+    }
+}
