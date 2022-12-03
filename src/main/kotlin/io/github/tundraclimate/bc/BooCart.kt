3479871cc3365c76b10d7843e0512b88f1b6de60
TundraClimate <tundra.tundere@gmail.com>
2022-12-03 23:12:41 +0900

    feat: add plugin getter

--- src/main/kotlin/io/github/tundraclimate/bc/BooCart.kt
+++ src/main/kotlin/io/github/tundraclimate/bc/BooCart.kt
@@ -1,6 +1,11 @@
 import org.bukkit.plugin.java.JavaPlugin
 
 class BooCart: JavaPlugin() {
+    private lateinit var plu: JavaPlugin
+    val plugin: JavaPlugin
+        get() = plu
+
     override fun onEnable() {
+        plu = this
     }
 }
