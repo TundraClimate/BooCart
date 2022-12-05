910cc061713b56baf45d6ff18aaf3bfa8c44fced
TundraClimate <tundra.tundere@gmail.com>
2022-12-05 23:33:36 +0900

    feat: HandleMap with ApplyProvider added

---
+++ src/main/kotlin/io/github/tundraclimate/bc/cart/CartHandleApply.kt
@@ -0,0 +1,37 @@
+package io.github.tundraclimate.bc.cart
+
+import io.github.tundraclimate.bc.BooCart
+import io.github.tundraclimate.bc.cart.control.HandleMap
+import io.github.tundraclimate.bc.config.NamespaceKeys
+import org.bukkit.NamespacedKey
+import org.bukkit.attribute.Attribute
+import org.bukkit.entity.LivingEntity
+import org.bukkit.persistence.PersistentDataType
+import org.bukkit.scheduler.BukkitRunnable
+import java.util.*
+
+class CartHandleApply {
+    private val applyRunnable = object : BukkitRunnable() {
+        override fun run() {
+            BooCart.plugin.server.worlds.forEach {
+                applySpeedByEntities(it.livingEntities)
+            }
+        }
+    }
+
+    private fun applySpeedByEntities(entities: List<LivingEntity>) = entities.forEach { applySpeed(it) }
+
+    private fun applySpeed(entity: LivingEntity) {
+        val container = entity.persistentDataContainer
+        if (container.has(NamespacedKey(BooCart.plugin, NamespaceKeys.IS_CART), PersistentDataType.BYTE)) {
+            val own = container.get(NamespacedKey(BooCart.plugin, NamespaceKeys.OWNER), PersistentDataType.STRING)
+                    ?: return
+            val (uuid, _) = own.split(":")
+            entity.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED)?.baseValue = HandleMap.getSpeed(UUID.fromString(uuid))
+        }
+    }
+
+    init {
+        applyRunnable.runTaskTimer(BooCart.plugin, 0, 10L)
+    }
+}
---
+++ src/main/kotlin/io/github/tundraclimate/bc/cart/control/HandleMap.kt
@@ -0,0 +1,28 @@
+package io.github.tundraclimate.bc.cart.control
+
+import io.github.tundraclimate.bc.BooCart
+import java.util.*
+
+object HandleMap {
+    private val map = mutableMapOf<UUID, Double>()
+
+    fun init(uuid: UUID) {
+        map[uuid] = 0.0
+    }
+
+    fun speedUp(uuid: UUID) {
+        map[uuid]?.let {
+            if (it + 0.25 > BooCart.conf.speed) return
+            map[uuid] = map[uuid]!! + 0.25
+        }
+    }
+
+    fun speedDown(uuid: UUID) {
+        map[uuid]?.let {
+            if (it - 0.25 < 0) return
+            map[uuid] = map[uuid]!! - 0.25
+        }
+    }
+
+    fun getSpeed(uuid: UUID) = map[uuid]!!
+}
