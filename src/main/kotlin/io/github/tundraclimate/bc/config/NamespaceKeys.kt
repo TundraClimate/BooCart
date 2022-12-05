41aae81b462ca67c8c2eee98177cff1d97a8cd16
TundraClimate <tundra.tundere@gmail.com>
2022-12-05 23:43:31 +0900

    feat: added Handle Namespace

--- src/main/kotlin/io/github/tundraclimate/bc/config/NamespaceKeys.kt
+++ src/main/kotlin/io/github/tundraclimate/bc/config/NamespaceKeys.kt
@@ -3,5 +3,6 @@ package io.github.tundraclimate.bc.config
 object NamespaceKeys {
     const val IS_CART = "cart"
     const val OWNER = "owner"
-    const val  EGG = "spawn_egg"
+    const val EGG = "spawn_egg"
+    const val HANDLE = "cart_handle"
 }
